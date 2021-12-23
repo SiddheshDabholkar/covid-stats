6695e01750f68fc1f4d0ea430cab6d8146a1bdc2
siddhesh <iamsiddhesh22@gmail.com>
2021-12-23 08:35:01 +0530

    installed required dependencies

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-//
+
 import Country from "./screens/Country";
 import Home from "./screens/Home";
+import SearchCountry from "./screens/SearchCountry";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,8 +19,9 @@ export default function App() {
             headerShown: false,
           }}
         >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Country" component={Country} />
+          <Stack.Screen name="SearchCountry" component={SearchCountry} />
+          {/* <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Country" component={Country} /> */}
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
